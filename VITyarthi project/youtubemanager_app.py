a290c3a9348efd2b341d8f1cbf7f0e4c4934701e
Jishanttanwar <jishanttanwar128@gmail.com>
2025-11-25 22:17:41 +0530

    Update youtubemanager_app.py

--- VITyarthi project/youtubemanager_app.py
+++ VITyarthi project/youtubemanager_app.py
@@ -1,32 +1,44 @@
 import json
+
 def data_load():
+    
     try:
         with open('youtube.txt','r') as file:
+
             test =  json.load(file)
             return(test)
+        
     except FileNotFoundError:
+        
         return[]
     
 def save_data(videos):
     with open('youtube.txt','w') as file:
         json.dump(videos, file)
 
- 
+
 def all_videos(videos):
+    
     print("\n")
-    print("-"* 195)      
+    print("-"* 50)      
     for index, video in enumerate(videos, start = 1):           
-        print(f"{index}.{video['name']}, Duration : {video['time']}")       
+        
+        print(index, video['name'], "Duration :" , video['time'])   
+    
     print("\n")
-    print("-"* 195)    
+    print("-"* 50)    
 
 def add_video(videos):
+    
     name = input("Enter video name :")
     time = input("Enter video time :")
     videos.append({'name': name , 'time': time})
+    
     save_data(videos)
 
-def Video_to_update(videos):
+
+def Video_update(videos):
+    
     if not videos:
         print("No video")
         return
@@ -36,19 +48,24 @@ def Video_to_update(videos):
         name = int(input("Enter a no. of video to be updated : "))
         if 1 <= name  <= len(videos):
             video = videos[name - 1]
-            print(f" Video name: {video['name']}")
-            print(f" Video Time : {video['time']} ")
+
+            print("Video name:", video["name"])
+           
+            print("Video Time:", video["time"])
 
             new_name = input("Enter new video name: ")
+            
             new_duration = input("Enter new video time : ")
 
             if new_name.strip():
                 video['name'] = new_name
+            
             if new_duration.strip():
                 video['time'] = new_duration
 
             save_data(videos)
             print("Video updated")
+        
         else:
             print("choice not valid.")
 
@@ -56,18 +73,23 @@ def Video_to_update(videos):
         print("Please enter a valid no.")
      
 
-def Video_to_delete(videos):
+def Video_delete(videos):
+    
     if not videos:
         print("There is no video that you added ")
+        
         return
 
     all_videos(videos)
+    
     try:
         video = int(input("Enter the  no. of the video to delete: "))
+        
         if 1 <= video <= len(videos):
             delete = videos.pop(video - 1)
             save_data(videos)
             print(f"Video deleted: {delete['name']}\n")
+        
         else:
             print("invalid request.")
     except ValueError:
@@ -88,15 +110,16 @@ def main():
         choice = input("Enter your choice : ")
         #print(videos)
     
+        
         match choice:
             case '1':
                 all_videos(videos)
             case '2':
                 add_video(videos)
             case '3':
-                Video_to_update(videos)
+                Video_update(videos)
             case '4':
-                Video_to_delete(videos)
+                Video_delete(videos)
             case '5':
                 break
             case _:
@@ -104,3 +127,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
